383282fa5dd873518ea2473f870e9da94dbb2b9f
hungnt2002 <hung0983330069@gmail.com>
2022-08-26 09:46:07 +0700

    Cập nhật powerpoint

--- src/Iterator/menuiterator/DinerMenu.java
+++ src/Iterator/menuiterator/DinerMenu.java
@@ -1,7 +1,7 @@
 package Iterator.menuiterator;
 
 public class DinerMenu {
-    private static final int MAX_ITEMS = 4;
+    static final int MAX_ITEMS = 4;
     private int numberOfItems = 0;
     private MenuItem[] menuItems;
 
